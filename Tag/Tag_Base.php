10677730f0a9bb7c5b76629dfcc9f8c7a8bdfb22
Hiroshi Kawai <hkawai@gmail.com>
2013-02-25 15:53:32 +0900

    Add comment.

--- Tag/Tag_Base.php
+++
@@ -1,3 +0,0 @@
-  public static function  () { return self::create(__FUNCTION__, func_get_args()); }
-  public static function o() { return self::create(__FUNCTION__, func_get_args()); }
-  public static function () { return self::create(__FUNCTION__, func_get_args()); }
